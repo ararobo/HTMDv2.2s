72098052a2c5cc27b9508c671d8652fdb0b4224d
hamuchan214 <hamuchan214@gmail.com>
2023-10-11 00:12:09 +0900

    update

--- HTMDv2.2sg431/Lib/Src/A3921Driver.cpp
+++ HTMDv2.2sg431/Lib/Src/A3921Driver.cpp
@@ -1,2 +1,29 @@
+/**
+  ******************************************************************************
+  * @file    A3921Driver.cpp
+  * @brief   A3921駆動用のプログラム
+  * @author @hamuchan214
+  * @date 2023/10/10
+  ******************************************************************************
+**/
+
 #include "stm32g4xx_hal.h"
+#include "tim.h"
 #include "math.h"
+
+class A3921Driver
+{
+  private:
+    //不変定数
+    const uint16_t _duty_max = 3999;
+    const uint16_t _duty_min = 1;
+    
+
+
+    //可変定数
+    uint16_t _duty;
+    bool _sr = 1;
+    bool _pwml=1;
+    bool _phase;
+
+}
