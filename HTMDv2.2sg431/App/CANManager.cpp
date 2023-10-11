fc09778137aa039469c95f910d58ea1cf97d6b8d
hamuchan214 <hamuchan214@gmail.com>
2023-10-12 00:01:30 +0900

    update
    CANマネージャの更新

--- HTMDv2.2sg431/App/CANManager.cpp
+++ HTMDv2.2sg431/App/CANManager.cpp
@@ -8,5 +8,91 @@
 **/
 
 #include "stm32g4xx_hal.h"
+#include "main.h"
 #include "fdcan.h"
 
+//データ宣言
+FDCAN_TxHeaderTypeDef  TxHeader;
+FDCAN_RxHeaderTypeDef  RxHeader;
+
+uint8_t TxData[8];
+uint8_t RxData[8];
+uint32_t TxMailbox;
+
+//CANのフィルタの宣言
+FDCAN_FilterTypeDef  sFilterConfig;
+
+//CANのフィルタ適用関数
+void CAN_FilterSetup(void)
+{
+    sFilterConfig.IdType = FDCAN_STANDARD_ID;
+    sFilterConfig.FilterIndex = 0;
+    sFilterConfig.FilterType = FDCAN_FILTER_MASK;
+    sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0; 
+    sFilterConfig.FilterID1 = 0x000;
+    sFilterConfig.FilterID2 = 0x000;
+
+    //設定の適用
+    if(HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+//CANの開始関数
+void CAN_Start(void)
+{
+  if(HAL_FDCAN_Start(&hfdcan1) != HAL_OK)//CANモジュールの開始
+  {
+    Error_Handler();
+  }
+
+  if (HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0) != HAL_OK)//CAN受信開始API呼び出し
+  {
+    Error_Handler();
+  }
+}
+
+//CAN送信関数
+void CAN_Start_Send(uint8_t id)
+{
+    TxHeader.Identifier =id;
+    TxHeader.IdType = FDCAN_STANDARD_ID;
+    TxHeader.TxFrameType = FDCAN_DATA_FRAME;
+    TxHeader.DataLength = FDCAN_DLC_BYTES_8;
+    TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+    TxHeader.BitRateSwitch = FDCAN_BRS_OFF;
+    TxHeader.FDFormat = FDCAN_CLASSIC_CAN;
+    TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+    TxHeader.MessageMarker = 0;
+    
+    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, TxData) != HAL_OK)
+	{
+		Error_Handler();
+	}
+    while(HAL_FDCAN_GetTxFifoFreeLevel(&hfdcan1) != 3) {}
+}
+
+
+// Message IDs
+#define STOP_COMMAND_ID           0x00F
+#define STOP_RESPONSE_ID          0x010
+#define OCD_NOTIFICATION_ID       0x040
+#define OVERHEAT_NOTIFICATION_ID  0x041
+
+// StopCommandの処理
+void Process_StopCommand(uint8_t* data)
+{
+    // データからMD_IDを取得
+    uint8_t md_id = data[0];
+
+    // 必要な処理をここで行う
+
+    // 応答としてStopResponseを送信
+    Send_StopResponse(md_id);
+}
+
+// StopResponseの送信
+void Send_StopResponse(uint8_t md_id)
+{
+}
