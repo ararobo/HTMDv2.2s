e99e0651980d1dfb481e08741a9e55c8e545e125
hamuchan214 <hamuchan214@gmail.com>
2023-10-12 12:01:08 +0900

    CANManagerの分割

---
+++ HTMDv2.2sg431/Module/Inc/CANManager.hpp
@@ -0,0 +1,43 @@
+/**
+  ******************************************************************************
+  * @file    CANManager.hpp
+  * @brief   Program for CAN communication
+  * @author @hamuchan214
+  * @date 2023/10/11
+  ******************************************************************************
+**/
+#ifndef CAN_MANAGER_HPP
+#define CAN_MANAGER_HPP
+
+#include "stm32g4xx_hal.h"
+#include "main.h"
+#include "fdcan.h"
+
+class CANManager
+{
+private:
+    static const uint16_t STOP_COMMAND_ID;
+    static const uint16_t STOP_RESPONSE_ID;
+    static const uint16_t OCD_NOTIFICATION_ID;
+    static const uint16_t OVERHEAT_NOTIFICATION_ID;
+    static const uint16_t INIT_WITH_CONF_ID;
+    static const uint16_t INIT_RESPONSE_ID;
+    static const uint16_t MOTOR_RUN;
+    static const uint16_t STATUS_REQUEST_ID;
+    static const uint16_t STATUS_RESPONSE_ID;
+
+    FDCAN_TxHeaderTypeDef  TxHeader;
+    FDCAN_RxHeaderTypeDef  RxHeader;
+    uint8_t TxData[8];
+    uint8_t RxData[8];
+    uint32_t TxMailbox;
+    FDCAN_FilterTypeDef  sFilterConfig;
+
+public:
+    void CAN_FilterSetup();
+    void CAN_Start();
+    void CAN_Start_Send(uint8_t id);
+    void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs);
+};
+
+#endif // CAN_MANAGER_HPP
---
+++ HTMDv2.2sg431/Module/Src/CANManager.cpp
@@ -0,0 +1,83 @@
+// CANManager.cpp
+
+#include "CANManager.hpp"
+
+// Initialize static const members
+const uint16_t CANManager::STOP_COMMAND_ID = 0x00F;
+const uint16_t CANManager::STOP_RESPONSE_ID = 0x010;
+const uint16_t CANManager::OCD_NOTIFICATION_ID = 0x040;
+const uint16_t CANManager::OVERHEAT_NOTIFICATION_ID = 0x041;
+const uint16_t CANManager::INIT_WITH_CONF_ID = 0x0FF;
+const uint16_t CANManager::INIT_RESPONSE_ID = 0x011;
+const uint16_t CANManager::MOTOR_RUN = 0x020;
+const uint16_t CANManager::STATUS_REQUEST_ID = 0x030;
+const uint16_t CANManager::STATUS_RESPONSE_ID = 0x031;
+
+void CANManager::CAN_FilterSetup()
+{
+    sFilterConfig.IdType = FDCAN_STANDARD_ID;
+    sFilterConfig.FilterIndex = 0;
+    sFilterConfig.FilterType = FDCAN_FILTER_MASK;
+    sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
+    sFilterConfig.FilterID1 = 0x000;
+    sFilterConfig.FilterID2 = 0x000;
+
+    // Apply the filter settings
+    if(HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void CANManager::CAN_Start()
+{
+    if(HAL_FDCAN_Start(&hfdcan1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    if (HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void CANManager::CAN_Start_Send(uint8_t id)
+{
+    TxHeader.Identifier = id;
+    TxHeader.IdType = FDCAN_STANDARD_ID;
+    TxHeader.TxFrameType = FDCAN_DATA_FRAME;
+    TxHeader.DataLength = FDCAN_DLC_BYTES_8;
+    TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+    TxHeader.BitRateSwitch = FDCAN_BRS_OFF;
+    TxHeader.FDFormat = FDCAN_CLASSIC_CAN;
+    TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+    TxHeader.MessageMarker = 0;
+
+    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &TxHeader, TxData) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    while(HAL_FDCAN_GetTxFifoFreeLevel(&hfdcan1) != 3) {}
+}
+
+void CANManager::HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
+{
+    if((RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) != RESET)
+        {
+            if (HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK)
+            {
+                Error_Handler();
+            }
+
+            switch (RxHeader.Identifier){
+                case STOP_COMMAND_ID:
+                    // Perform operations for ID 0x000
+                    break;
+                case 0x001:
+                    // Perform operations for ID 0x001
+                    break;
+            }
+        }
+}
